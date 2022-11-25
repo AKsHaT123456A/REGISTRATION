6a5349dd06c685ceaec4d34269cf3873c8d387b9
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2022-11-25 22:04:36 +0530

    Deployment

--- controllers/user.js
+++ controllers/user.js
@@ -6,10 +6,12 @@ const login = require("../routes/login");
 const logout = require("../routes/logout");
 const auth = require("../middleware/auth");
 const otpController = require("../routes/otp");
+const sendResetMail=require("../routes/reset");
 router.post("/register",register);
 router.post("/login",login);
 router.get("/home",auth);
 router.get("/logout",auth,logout);
-router.get("/otp",otpController.otpChecker)
+router.get("/otp",otpController.otpChecker);
+router.get("/reset",sendResetMail)
 module.exports = router;
 
---
+++ routes/reset.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const app = express();
+const userSchema= require("../models/userSchema");
+const { json } = require("body-parser");
+express.json({extended:true});
+const userData = mongoose.model("userData",userSchema);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json())
+
+const sendResetMail = async (req,res)=>{
+    try {
+        const email = req.body.email;
+         userData.findOne({email},function(err,result){
+
+        
+          let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        requireTLS:true,
+        auth: {
+          user: process.env.SENDER_KEY, 
+          pass: process.env.EMAIL_TEST_PASSWORD,  
+        },
+      });
+    
+      let info =  transporter.sendMail({
+        from:process.env.SENDER_KEY,
+        to:email,
+        subject:"RESET", 
+        text: "The password is:-", 
+         html: result.password2
+      });  
+      console.log("sent");
+    });
+     
+    }catch (error) {
+      console.log(error);
+    //   res.json({
+    //       status:"FAILED",
+    //       message:"Error Occured in saving"
+    //   });
+        }};  
+        module.exports=sendResetMail;

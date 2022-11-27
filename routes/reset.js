925db3b30015f635d7f4c902932c4e039a95a256
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2022-11-27 19:29:24 +0530

    Deployment

--- routes/reset.js
+++ routes/reset.js
@@ -3,7 +3,16 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
+const cors = require("cors");
 const app = express();
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+app.use(cors());
 const userSchema= require("../models/userSchema");
 // express.json({extended:true});
 const userData = mongoose.model("userData",userSchema);

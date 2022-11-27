cc1fe8e4cf0eeab7ad502a4c8e131ae9a7787e25
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2022-11-27 20:23:44 +0530

    Deployment

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,5 @@
 const express = require("express"); 
 const router =express();
-const v=0;
 const bodyParser = require("body-parser");
 const register = require("../routes/register");
 const login = require("../routes/login");

15f8b3f1094b5ce5988f742b97c1e55149a40a5b
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2022-11-28 17:37:11 +0530

    Deployment

--- routes/otp.js
+++ routes/otp.js
@@ -81,13 +81,6 @@ const sendVerifyMail = async (_id,email,res)=>{
               res.send("Valid");
              }
         }
-      
-        else{
-         res.send("Enter the otp");
-          //  console.log(userOtpRecord);
-        //    console.log(hashedOtp);
-
-        }
       } catch (error) {
         console.log(error);
       }
